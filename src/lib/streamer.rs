5cbaae7c50eb765df6b0f1c5da87a7365ed90a75
scadoshi <scottyfermo@hotmail.com>
2026-01-30 16:04:56 -0800

    Resolved issue with threads not seeing when the other has quit with Arc<AtomicBool>

--- src/lib/streamer.rs
+++ src/lib/streamer.rs
@@ -1,6 +1,10 @@
 use std::{
     io::{stdin, stdout, BufRead, BufReader, Write},
     net::TcpStream,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
     thread,
     time::Duration,
 };
@@ -11,22 +15,34 @@ impl Streamer {
         let mut writer = stream.try_clone()?;
         stream.set_read_timeout(Some(Duration::from_millis(100)))?;
         let mut reader = BufReader::new(stream);
-        let (ender, end_signal) = std::sync::mpsc::channel::<bool>();
+        let reader_end_handle = Arc::new(AtomicBool::new(false));
+        let writer_end_handle = reader_end_handle.clone();
         // reader
         let reader_thread = thread::spawn(move || -> anyhow::Result<()> {
             loop {
-                let mut should_end = false;
-                if let Ok(signal) = end_signal.try_recv() {
-                    should_end = signal;
-                }
                 let mut line = String::new();
-                let content = reader.read_line(&mut line)?;
-                if should_end || content == 0 || line.trim() == "/exit" {
-                    println!("They exited");
-                    break;
+                match reader.read_line(&mut line) {
+                    Ok(0) => break,
+                    Ok(content) => {
+                        if reader_end_handle.load(Ordering::Relaxed)
+                            || content == 0
+                            || line.trim() == "/exit"
+                        {
+                            println!("They exited");
+                            reader_end_handle.store(true, Ordering::Relaxed);
+                            break;
+                        }
+                        println!("Received: {}", line.trim());
+                        stdout().flush()?;
+                    }
+                    Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                        if reader_end_handle.load(Ordering::Relaxed) {
+                            break;
+                        }
+                        continue;
+                    }
+                    Err(e) => return Err(e.into()),
                 }
-                println!("Received: {}", line.trim());
-                stdout().flush()?;
             }
             Ok(())
         });
@@ -35,16 +51,20 @@ impl Streamer {
             'main: loop {
                 println!("Type a message and click enter to send");
                 for line in stdin().lines() {
-                    let line = line?;
-                    if line.trim() == "/exit" {
-                        println!("Exiting");
-                        ender.send(true)?;
+                    if writer_end_handle.load(Ordering::Relaxed) {
+                        println!("They exited");
                         break 'main;
                     }
+                    let line = line?;
                     writer.write_all(line.as_bytes())?;
                     writer.write_all(b"\n")?;
                     writer.flush()?;
                     println!("Message sent");
+                    if line.trim() == "/exit" {
+                        println!("Exiting");
+                        writer_end_handle.store(true, Ordering::Relaxed);
+                        break 'main;
+                    }
                 }
             }
             Ok(())
